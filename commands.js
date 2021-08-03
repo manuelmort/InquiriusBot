68536e554ae1263bc9f050664c346f452ae31db5
Manuel Morteo <42097353+manuelmort@users.noreply.github.com>
2021-08-03 13:05:57 -0700

    Delete commands.js

--- commands.js
+++
@@ -1,7 +0,0 @@
-var mongo = require('mongodb').MongoClient
-var url = "mongodb+srv://bluerare:manuel09!@cluster0.4zhfz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const { channel } = require('diagnostics_channel');
-const Discord = require('discord.js');
-
-
-
